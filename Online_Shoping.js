7b66167672dc85687f5d059cc7c6cf022f3fd5f8
AnkurSharma7893 <ankursharmass_cse17@its.edu.in>
2025-06-10 14:14:13 +0530

    Update Online_Shoping.js

--- Online_Shoping.js
+++ Online_Shoping.js
@@ -2,7 +2,7 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
-if(bar){
+if(bar){    
     bar.addEventListener('click',() =>{
       nav.classList.add('active');  
     })
